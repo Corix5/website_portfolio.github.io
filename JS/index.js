ed82660d318c75833c512b1109f18e1a08e31bca
Luis Coria <luisdavid202075@gmail.com>
2023-07-31 18:02:28 -0600

    Se agregaron animaciones de transicion

---
+++ JS/index.js
@@ -0,0 +1,91 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.box_container', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.chat', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.terminal-scroll', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '400px'
+});
+
+sr.reveal('.education_container', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '200px'
+});
+
+sr.reveal('.test_description_text', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.test_figure', {
+    duration: 2000,
+    origin: 'right',
+    distance: '100px'
+});
+
+sr.reveal('.testimonial_container', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('.tech_description_text', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.tech_figure', {
+    duration: 2000,
+    origin: 'right',
+    distance: '100px'
+});
+
+sr.reveal('.technologies_container', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('.proj_title', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.projects_image', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('#list_projects', {
+    duration: 2000,
+    origin: 'right',
+    distance: '100px'
+});
+
+sr.reveal('.ul_projects', {
+    duration: 1999,
+    origin: 'left',
+    distance: '300px'
+});
+
+sr.reveal('.projects', {
+    duration: 1999,
+    origin: 'bottom',
+    distance: '100px'
+});
